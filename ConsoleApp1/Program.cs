460c784d5d0822afcdd1dd9d13e915d48844d2ef
Michael Peter <michael_piterct@hotmail.com>
2021-07-19 11:12:44 -0300

    initial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digit a word");
+
+            var objeto = Console.ReadLine();
+
+            string newValue = string.Empty;
+            int i = 1;
+            foreach( var value in objeto)
+            {               
+                 newValue += objeto[objeto.Length - i];
+                i++;
+            }
+
+            if(objeto == newValue)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
